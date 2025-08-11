d5cad8e350688bd3568a5353da65fad653de19e2
Harshit Saxena <dev.harshit@proton.me>
2025-08-11 18:31:55 +0530

    Started Project And First Commit

---
+++ build/CMakeFiles/meow.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/meow.dir/src/main.cpp.o"
+  "CMakeFiles/meow.dir/src/main.cpp.o.d"
+  "meow"
+  "meow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/meow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc,char* argv[]){
+	if(argc!=2){
+	std::cout<<"Wrong Usage Please Use Like Given Below!!\n";
+	std::cout<<"meow <filename>.kitty";
+}
+	return 0;	
+
+	}
